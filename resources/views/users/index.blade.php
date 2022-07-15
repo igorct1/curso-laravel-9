1f089f366df5d44f646b7c012bf443270aaf7fc5
Igor Tozetti <igorct77@gmail.com>
2022-07-14 23:01:13 -0300

    how to work with blade templates

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,15 @@
-<h1>Listagem dos usuários</h1>
-<ul>
-    @foreach ($users as $user)
-        <li>{{ $user->name }} -
-            {{ $user->email }} |
-            <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
-        </li>
-    @endforeach
-</ul>
+{{-- receber template --}}
+@extends('layouts.app')
+{{-- title --}}
+@section('title', 'Listagem dos usuários')
+@section('content')
+    <h1>Listagem dos usuários</h1>
+    <ul>
+        @foreach ($users as $user)
+            <li>{{ $user->name }} -
+                {{ $user->email }} |
+                <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,11 @@
-<h1>Listagem do usuario {{ $user->name }}</h1>
-
-<ul>
-    <li>{{ $user->name }}</li>
-    <li>{{ $user->email }}</li>
-</ul>
+{{-- receber template --}}
+@extends('layouts.app')
+{{-- title --}}
+@section('title', 'Listagem do usuário')
+@section('content')
+    <h1>Listagem do usuario {{ $user->name }}</h1>
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+    </ul>
+@endsection
